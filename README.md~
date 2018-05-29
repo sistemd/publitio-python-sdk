8ffc0f48618fb77ac37188e00c5d44340c3bdbdd
ennmichael <enntheprogrammer@gmail.com>
2018-05-29 17:14:08 +0200

    Finished the API and added some docs

---
+++ README.md~
@@ -0,0 +1,190 @@
+#Publitio Python SDK
+
+To use the Python SDK:
+
+```
+from publitio import PublitioAPI
+publitio_api = PublitioAPI(key='xxxx', secret='yyyy')
+```
+
+For the remaning code samples, these two lines of code will be implied.
+
+The methods used for communicating with the api - `create_file`, `list_files` etc. all return parsed json responses, as if `json.loads` was used on the response content. The only exception to this is the `transformed` method, which returns `bytes`. In all methods, the supported keyword parameters are exactly the same as in the publitio docs: https://publit.io/docs. You should probably look into that documentation before reading this.
+
+
+##Exceptions
+
+
+##Transforming files
+```
+publitio_api.transform('filename.jpeg', extension='png', w=300, h=300)
+```
+
+This would return the `bytes` of the file named `filename`, now transcoded into a png, with dimensions 300x300. The supported parameters are the same as in https://publit.io/docs/#url-based-transformations. For example, the above invocation makes a call to this URL:
+```
+https://media.publit.io/file/w_300,h_300/filename.png
+```
+
+
+##Creating files
+```
+publitio_api.create_file(file=open('path/to/file', 'rb'),
+                         title='My title',
+                         description='My description')
+```
+Please do note that the file must be opened for binary reading. **The function will not close the file**. Therefore, what you will probably want to do most of the time is:
+```
+with open('path/to/file', 'rb') as f:
+    publitio_api.create_file(file=open('path/to/file', 'rb'),
+                             title='My title',
+                             description='My description')
+```
+
+
+##Listing files
+```
+publitio_api.list_files(offset=1, ...)
+```
+
+It's also OK to provide strings as parameters when numbers seem logical. So, also valid:
+
+```
+publitio_api.list_files(offset='1', ...)
+```
+
+
+##Showing files
+```
+publitio_api.show_file(file_id)
+```
+
+
+##Updating files
+```
+publitio_api.update_file(file_id, title='A better title', ...)
+```
+
+
+##Deleting files
+```
+publitio_api.delete_file(file_id)
+```
+
+
+##Getting a file player
+```
+publitio_api.get_file_player(file_id, player='myplayerid', ...)
+```
+
+
+##Creating file versions
+```
+publitio_api.create_version(file_id, extension='.webm', ...)
+```
+
+
+##Listing versions
+```
+publitio_api.list_versions(file_id, limit=3)  # Or limit='3'
+```
+
+
+##Showing versions
+```
+publitio_api.show_version(version_id)
+```
+
+
+##Deleting versions
+```
+publitio_api.delete_version(version_id)
+```
+
+
+##Creating players
+```
+publitio_api.create_player(name='whatever', ...)
+```
+
+
+##Listing players
+```
+publitio_api.list_players()
+```
+
+
+##Showing players
+```
+publitio_api.show_player(player_id)
+```
+
+
+##Updaing players
+```
+publitio_api.update_player(player_id, skin='green', ...)
+```
+
+
+##Deleting players
+```
+publitio_api.delete_player(player_id)
+```
+
+
+##Creating adtags
+```
+publitio_api.create_adtag(name='name', tag='tag')
+```
+
+
+##Listing adtags
+```
+publitio_api.list_adtags()
+```
+
+
+##Showing adtags
+```
+publitio_api.show_adtag(adtag_id)
+```
+
+
+##Updating adtags
+```
+publitio_api.update_adtag(adtag_id, tag='newtag')
+```
+
+
+##Deleting adtags
+```
+publitio_api.delete_adtag(adtag_id)
+```
+
+
+##Creating a watermark
+```
+publitio_api.create_watermark(self, file=open('path/to/file', rb), title='whatever')
+```
+Again, the file needs to be opened for binary reading. This function will not close the file.
+
+
+##Listing watermarks
+```
+publitio_api.list_watermarks()
+```
+
+##Showing watermarks
+```
+publitio_api.show_watermark(watermark_id)
+```
+
+
+##Updating watermarks
+```
+publitio_api.update_watermark(watermark_id, position='left', ...)
+```
+
+##Deleting watermarks
+```
+publitio_api.delete_watermark(watermark_id)
+```
--- publitio.py
+++ publitio.py
@@ -1,25 +1,26 @@
-import contextlib
 import random
 import hashlib
 import calendar
-import abc
-import io
 import functools
+import os.path
 from datetime import datetime
 
 import requests
 
 
-__all__ = ['PublitioAPI', 'AsyncPublitioAPI']
-
-
-API_URL = 'https://api.publit.io/v1/'
+__all__ = ['UnknownStatusCode', 'TransformationFailed', 'PublitioAPI']
 
 
 class UnknownStatusCode(BaseException):
 
     def __init__(self, status_code):
         super().__init__('API call returned unknown status code {}.'.format(status_code))
+
+
+class TransformationFailed(BaseException):
+
+    def __init__(self, reason):
+        super().__init__('File transformation failed. Resonse: ' + reason)
 
 
 def generate_nonce():
@@ -47,19 +48,33 @@ def parse_response(response):
     raise UnknownStatusCode
 
 
-def safe_request(method, *args, **kwargs):
+def rest_request(method, *args, **kwargs):
     res = method(*args, **kwargs)
-    check_response(res)
+    check_status_code(res.status_code)
     return res.json()
 
 
-safe_get = functools.partial(safe_request, requests.get)
-safe_put = functools.partial(safe_request, requests.put)
-safe_post = functools.partial(safe_request, requests.post)
-safe_delete = functools.partial(safe_request, requests.delete)
+def filename_without_extension(filename):
+    return os.path.splitext(filename)[0]
+
+
+def replace_extension(filename, new_extension):
+    return '{}.{}'.format(filename_without_extension(filename),
+                          new_extension)
+
+
+rest_get = functools.partial(rest_request, requests.get)
+rest_put = functools.partial(rest_request, requests.put)
+rest_post = functools.partial(rest_request, requests.post)
+rest_delete = functools.partial(rest_request, requests.delete)
 
 
 class PublitioAPI:
+    '''A class to communicate with the publitio API.
+    '''
+
+    API_URL = 'https://api.publit.io/v1/'
+    MEDIA_URL = 'https://media.publit.io/'
 
     def __init__(self, key, secret):
         self.key = key
@@ -88,26 +103,29 @@ class PublitioAPI:
         result.update(user_payload)
         return result
 
-    def api_get(self, path, payload):
-        return safe_get(API_URL + path,
-                        params=self.full_payload(payload))
+    def api_get(self, path, payload=None):
+        return rest_get(PublitioAPI.API_URL + path,
+                        params=self.full_payload(payload or {}))
 
-    def api_put(self, path, data):
-        return safe_put(API_URL + path,
+    def api_put(self, path, data=None):
+        return rest_put(PublitioAPI.API_URL + path,
                         params=self.api_payload(),
-                        data=data)
+                        data=data or {})
 
-    def api_post(self, path, payload, data):
-        return safe_post(API_URL + path,
+    def api_post(self, path, payload=None, data=None):
+        payload = payload or {}
+        data = data or {}
+
+        return rest_post(PublitioAPI.API_URL + path,
                          params=self.full_payload(payload),
                          files=data)
 
     def api_delete(self, path):
-        return safe_delete(API_URL + path,
+        return rest_delete(PublitioAPI.API_URL + path,
                            params=self.api_payload())
 
 
-    def upload_file(self, file, **params):
+    def create_file(self, file, **params):
         return self.api_post('files/create',
                              payload=params,
                              data={'file': file})
@@ -124,11 +142,100 @@ class PublitioAPI:
     def delete_file(self, file_id):
         return self.api_delete('files/delete/' + file_id)
 
-    def player_file(self, file_id, **params):
+    def get_file_player(self, file_id, **params):
         return self.api_get('files/player/' + file_id, params)
 
+    def create_version(self, file_id, **params):
+        return self.api_post('files/versions/create/' + file_id,
+                             payload=params)
 
-EXAMPLE_FILE_ID = '2P65LMzD'
+    def list_versions(self, file_id, **params):
+        return self.api_get('files/versions/list/' + file_id,
+                            payload=params)
+
+    def show_version(self, version_id):
+        return self.api_get('files/versions/show/' + version_id)
+
+    def delete_version(self, version_id):
+        return self.api_delete('files/versions/delete/' + version_id)
+
+    def create_player(self, **params):
+        return self.api_post('players/create', payload=params)
+
+    def list_players(self):
+        return self.api_get('players/list')
+
+    def show_player(self, player_id):
+        return self.api_get('players/show/' + player_id)
+
+    def update_player(self, player_id, **params):
+        return self.api_put('players/update/' + player_id, data=params)
+
+    def delete_player(self, player_id):
+        return self.api_delete('players/delete/' + player_id)
+
+    def create_adtag(self, **params):
+        return self.api_post('players/adtags/create', payload=params)
+
+    def list_adtags(self):
+        return self.api_get('players/adtags/list')
+
+    def show_adtag(self, adtag_id):
+        return self.api_get('players/adtags/show/' + adtag_id)
+
+    def update_adtag(self, adtag_id, **params):
+        return self.api_put('players/adtags/update/' + adtag_id,
+                            data=params)
+
+    def delete_adtag(self, adtag_id):
+        return self.api_delete('players/adtags/delete/' + adtag_id)
+
+    def create_watermark(self, file, **params):
+        '''Make sure the file is opened for binary reading.
+        '''
+        return self.api_post('watermarks/create',
+                             payload=params,
+                             data={'file': file})
+
+    def list_watermarks(self):
+        return self.api_get('watermarks/list')
+
+    def show_watermark(self, watermark_id):
+        return self.api_get('watermarks/show/' + watermark_id)
+
+    def update_watermark(self, watermark_id, **params):
+        return self.api_put('watermarks/update/' + watermark_id,
+                            data=params)
+
+    def delete_watermark(self, watermark_id):
+        return self.api_delete('watermarks/delete/' + watermark_id)
+
+
+    @staticmethod
+    def transformation_options(**params):
+        return ','.join('{}_{}'.format(k, v) for k, v in params.items())
+
+    @staticmethod
+    def transformation_url(filename, *, extension, **params):
+        filename = replace_extension(filename, extension)
+        options = PublitioAPI.transformation_options(**params)
+        options_url = options + '/' if options else ''
+        return PublitioAPI.MEDIA_URL + 'file/' + options_path + filename
+
+    def transformed(self, filename, *, extension, **params):
+        '''Pass in parameters like: w=33, h=45
+        '''
+        url = PublitioAPI.transformation_url(filename,
+                                             extension=extension,
+                                             **params)
+
+        res = requests.get(url)
+        if not res.ok:
+            raise TransformationFailed(res.reason)
+        return res.content
+
+
+EXAMPLE_FILE_ID = 'cq4ReLGx'
 
 
 if __name__ == '__main__':
@@ -137,4 +244,4 @@ if __name__ == '__main__':
     publitio_api = PublitioAPI(key='ktuZkDrpfA3M7t3txAp0',
                                secret='RWnZpAdRa8olrNaDjsZp1Q5VbWgznwy8')
 
-    pprint(publitio_api.upload_file(file=io.open('/home/jimihendrix/index.jpeg', 'rb'), title='Whatever'))
+    pprint(publitio_api.list_files(offset=1))
